4055017ee1f1906c6d24b8a71d19546ad5295bed
705Marc <Marc.Montigny@outlook.de>
2026-04-02 18:34:10 +0200

    Refactor kiosk configuration for improved clarity and functionality

--- common/kiosk.nix
+++ common/kiosk.nix
@@ -1,28 +1,37 @@
 { pkgs, ... }:
 
 {
-  # Grafik-Support
+  # 1. Grafik & VMware Treiber
   hardware.graphics.enable = true;
+  virtualisation.vmware.guest.enable = true;
 
-  # Kiosk-User anlegen
+  # 2. Kiosk-User mit allen nötigen Rechten
   users.users.kiosk = {
     isNormalUser = true;
-    extraGroups = [ "video" ];
+    extraGroups = [ "video" "render" "input" ];
   };
 
-  # Autologin auf tty1 (damit Cage sofort startet)
-  services.getty.autologinUser = "kiosk";
+  # 3. Den Text-Login auf TTY1 deaktivieren (sehr wichtig!)
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
+  # 4. Cage Kiosk Konfiguration
   services.cage = {
     enable = true;
     user = "kiosk";
-    # Umgebungsvariable für Software-Rendering in VMs hinzufügen
-    extraArguments = [ "-s" ]; 
+    tty = 1; # Festlegen auf TTY1
+    
+    # Der absolute Pfad zu Firefox
     program = "${pkgs.firefox}/bin/firefox --kiosk https://nixos.org";
+    
+    # Diese Variablen sind der "Joker" für VMs
+    environment = {
+      WLR_RENDERER_ALLOW_SOFTWARE = "1"; # Erzwingt Start auch ohne 3D-Beschleunigung
+      WLR_NO_HARDWARE_CURSORS = "1";     # Behebt oft unsichtbare Mauszeiger in VMware
+      XDG_SESSION_TYPE = "wayland";
+    };
   };
 
-  # WICHTIG für VMware: Gast-Erweiterungen aktivieren
-  virtualisation.vmware.guest.enable = true;
-
+  # 5. Firefox installieren
   environment.systemPackages = [ pkgs.firefox ];
 }
