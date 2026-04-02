da247a657735b7db8834eef2d9396dfda8d22f11
705Marc <Marc.Montigny@outlook.de>
2026-04-02 18:40:28 +0200

    Refactor kiosk configuration comments for improved clarity and consistency

--- common/kiosk.nix
+++ common/kiosk.nix
@@ -1,36 +1,43 @@
 { pkgs, ... }:
 
 {
-  # 1. Grafik & VMware Treiber
+  # 1. Grundvoraussetzung für Grafik
   hardware.graphics.enable = true;
   virtualisation.vmware.guest.enable = true;
 
-  # 2. Kiosk-User mit allen nötigen Rechten
+  # 2. User-Rechte erweitern
   users.users.kiosk = {
     isNormalUser = true;
     extraGroups = [ "video" "render" "input" ];
   };
 
-  # 3. Den Text-Login auf TTY1 deaktivieren (sehr wichtig!)
+  # 3. Text-Login auf TTY1 deaktivieren
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
-  # 4. Cage Kiosk Konfiguration
+  # 4. Cage Konfiguration
   services.cage = {
     enable = true;
     user = "kiosk";
     program = "${pkgs.firefox}/bin/firefox --kiosk https://nixos.org";
+    
     environment = {
-      # WICHTIG: Sagt Firefox, dass er Wayland nutzen soll
+      # WICHTIG: Firefox für Wayland
       MOZ_ENABLE_WAYLAND = "1";
-      # Erlaubt Software-Rendering in der VM
+      # VM-Kompatibilität
       WLR_RENDERER_ALLOW_SOFTWARE = "1";
       WLR_NO_HARDWARE_CURSORS = "1";
+      # Standard-Wayland Variablen
       XDG_SESSION_TYPE = "wayland";
-      XDG_CURRENT_DESKTOP = "cage";
+      XDG_SESSION_CLASS = "user";
+      # Damit Firefox weiß, wo er den Grafik-Socket findet
+      XDG_RUNTIME_DIR = "/run/user/1001"; # 1001 ist die ID von User 'kiosk'
     };
   };
 
-  # 5. Firefox installieren
-  environment.systemPackages = [ pkgs.firefox ];
+  # 5. Notwendige Pakete
+  environment.systemPackages = with pkgs; [ 
+    firefox
+    mesa # Für die Grafik-Bibliotheken
+  ];
 }
